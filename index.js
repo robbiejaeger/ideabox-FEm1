895b4741b7c46fc86b4c6fc85f6054c14292ee21
Robbie Jaeger <jaeger.rob@gmail.com>
2016-12-17 07:47:34 -0700

    Add delete and upvote functionality

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-var $titleInput = $('#title-input');
-var $bodyInput = $('#body-input');
-var $saveBtn = $('#save-btn');
+var $titleInput = $('#title-input')
+var $bodyInput = $('#body-input')
+var $saveBtn = $('#save-btn')
 var $ideasContainer = $('.ideas-container')
+var ideaQualities = ['swill', 'plausible', 'genius']
 
 $saveBtn.on('click', function(e){
   e.preventDefault()
@@ -11,7 +12,7 @@ $saveBtn.on('click', function(e){
 })
 
 function getUserInput(){
-  return {title: $titleInput.val(), body: $bodyInput.val()};
+  return {title: $titleInput.val(), body: $bodyInput.val()}
 }
 
 function ideaTemplate(inputText){
@@ -21,14 +22,55 @@ function ideaTemplate(inputText){
     <p class="idea-body">${inputText.body}</p>
     <input class="vote-btn upvote-btn" type="button">
     <input class="vote-btn downvote-btn" type="button">
-    <p class="idea-quality">
-      quality: swill
+    <p class="idea-quality" data-quality="0">
+      quality: <span class="quality-val-text">${ideaQualities[0]}</span>
     </p>
     <hr class="idea-divider">
   </article>`
 }
 
 function clearInputs(){
-  $titleInput.val("");
-  $bodyInput.val("");
+  $titleInput.val('')
+  $bodyInput.val('')
+}
+
+$ideasContainer.on('click', '.delete-btn', function(){
+  $(this).parents('.idea').remove()
+})
+
+$ideasContainer.on('click', '.upvote-btn', upvote)
+
+$ideasContainer.on('click', '.downvote-btn', downvote)
+
+function upvote(){
+  var $qualityEl = $(this).siblings('.idea-quality')
+  var currentQuality = $qualityEl.data('quality')
+
+  switch (currentQuality) {
+    case 0:
+      changeQuality($qualityEl, 1)
+      break
+    case 1:
+      changeQuality($qualityEl, 2)
+      break
+  }
+}
+
+function downvote(){
+  var $qualityEl = $(this).siblings('.idea-quality')
+  var currentQuality = $qualityEl.data('quality')
+
+  switch (currentQuality) {
+    case 1:
+      changeQuality($qualityEl, 0)
+      break
+    case 2:
+      changeQuality($qualityEl, 1)
+      break
+  }
+}
+
+function changeQuality(qualityElement, val){
+  qualityElement.data('quality', val)
+  qualityElement.children('.quality-val-text').text(ideaQualities[val])
 }
